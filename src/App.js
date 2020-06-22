13fbbed2851549a987329d941afad35f58b85cc9
GimpHJ <devhjlim@gamil.com>
2020-06-21 18:41:00 -0700

    still working on authentication.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Homepage from './pages/homepage/hompage.component'
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 import { Route } from 'react-router-dom';
 
@@ -13,6 +14,7 @@ function App() {
     <switch>
       <Route exact path='/' component={Homepage}></Route>
       <Route path='/shop' component={ShopPage}></Route>
+      <Route path='/signin' component={SignInAndSignUpPage}></Route>
     </switch>
   </div>
 }
