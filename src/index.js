15e5b643fdd93bb691609dd8121fe12477d5e6e4
GimpHJ <devhjlim@gamil.com>
2020-07-05 15:02:00 -0700

    added redux persist, use local storate to persist state data.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
+
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
-  <BrowserRouter>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </BrowserRouter>
+    <BrowserRouter>
+      <React.StrictMode>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </React.StrictMode>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
