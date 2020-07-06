100d2342e75151a96ee063462d68fc2cd90bcefb
GimpHJ <devhjlim@gamil.com>
2020-07-05 20:11:22 -0700

    moved directory js and stop data js into reducer. created component and selectors. /shop

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,17 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
-import SHOP_DATA from './shop.data';
 
-import CollectionPreivew from '../../components/collection-preview/collection-preview.component';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-class ShopPage extends React.Component{
-    constructor(props){
-        super(props);
 
-        this.state = {
-            collections:SHOP_DATA
-        };
-    }
+const ShopPage = ({ match }) => (
+    <div className='shop-page'>
+        <Route exact path={`${match.path}`} component={CollectionsOverview}></Route>
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage}></Route>
+    </div>
+);
 
-    render(){
-        const {collections} = this.state;
-        return (<div className='shop-page'>
-            {
-                collections.map(({ id, ...otherCollectionProps})=>(
-                    <CollectionPreivew key={id} {...otherCollectionProps} />
-                ))
-            }
-        </div>);
-    }
-}
 
 export default ShopPage;
